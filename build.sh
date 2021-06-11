fd024ba9134c61a86ab5962d6240922fa802459d
ilya.melnikov <ilya.melnikov@digitalplant.ru>
2021-06-11 17:28:13 +0300

    build: version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-tag=healthcheck-exporter:0.1.10
+tag=healthcheck-exporter:0.1.11
 
 echo Building $tag
 docker build --no-cache -t ziiot-docker.dp.nlmk.com/digital-plant/$tag . -f ./Dockerfile
